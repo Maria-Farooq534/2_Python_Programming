f17b5e8e8a1213cb445a4a8e4a8489dacaf27e00
Maria <mariafarooq164@gmail.com>
2026-06-12 18:09:42 +0500

    user_input

---
+++ basic_projects/1_user_info.py
@@ -0,0 +1,19 @@
+# Create a program that:
+
+# asks name
+# asks university
+# asks CGPA
+
+# Outputs:
+
+# Hello Maria
+# You study at XYZ University
+# Your CGPA is 3.8
+
+name = input("Enter your Name: ")
+uni = input("Enter University Name: ")
+cgpa = float(input("Enter your CGPA: "))
+
+print(f"Hello {name}!")
+print(f"You study at {uni} University.")
+print(f"Your CGPA is {cgpa}")
