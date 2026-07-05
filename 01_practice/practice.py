07ef6e377a334a032214e665c897945c7cd3e6e6
Maria <mariafarooq164@gmail.com>
2026-07-05 18:28:46 +0500

    Improved Project 02: Student Result Management System

--- 01_practice/practice.py
+++ 01_practice/practice.py
@@ -1,63 +1,110 @@
-# Mini Project
-# Create a simple Calculator using functions.
-# Functions:
-# add(a, b)
-# subtract(a, b)
-# multiply(a, b)
-# divide(a, b)
+# ========== Student Result Management ==========
+# 1. Add Student
+# 2. View All Students
+# 3. Search Student
+# 4. Update CGPA
+# 5. Delete Student
+# 6. Exit
 
-# Each function should return the answer, not print it.
+student_list = []
+def add_std(std):
+    print(f"{std} record added successfuly.")
+    return student_list.append(std)
+    
 
-# Add
-def addition(num1, num2):
-    return num1 + num2
+def view_std(std):    
+    for student in student_list:
+        print(f"The records for {student["name"].title()}: ")
+        for key, value in student.items():
+            print(f"{key} : {value}")
+        print("-" * 30)
+    if not student_list:
+        print(f"No record found!")    
+        
+def search_std(std):
+    found = False
+    for student in student_list:
+        if student["name"].lower() == std:
+            print(f"{std.title()}'s record found: ")
+            for key, value in student.items():
+                print(f"{key} : {value}")
+            found = True
+            break
+    if not found:
+        print(f"No record found for {std.title()}!")
+                
+def update_cgpa(std):
+    found = False
+    for student in student_list:
+        if student["name"].lower() == std:
+            updated_cgpa = float(input("Enter updated CGPA: "))
+            student["cgpa"] = updated_cgpa
+            print(f"{std.title()}'s CGPA updated successfully!")
+            found = True
+            break
+        if not found:
+            print(f"Invalid choice! {std.title()}'s record is not available. Please select from available records!")
+                      
 
-def subtraction(num1, num2):
-    return num1 - num2
-
-def multiplication(num1, num2):
-    return num1 * num2
-
-def division(num1, num2):
-    return num1 / num2
-def power(num1, num2):
-    return num1 ** num2
+def delete_std(std):
+    found = False
+    for student in student_list:
+        if student["name"].lower() == std:
+            student_list.remove(student)
+            print(f"{student["name"].title()}'s record deleted successfully!")
+            found = True
+            break
+    if not found:
+        print(f"{std.title()}'s record didn't exists. Please select from avaialble records!")
+            
+             
 
 while True:
+    
     print("""
-      Select a number for its corresponding operation:
-      1. Add
-      2. Subtract
-      3. Multiplication
-      4. Division
-      5. Power
-      6. Exit
-      """)
+    ========== Student Result Management ==========
+    1. Add Student
+    2. View All Students
+    3. Search Student
+    4. Update CGPA
+    5. Delete Student
+    6. Exit
+    """)
     
-    user_operation = int(input("Enter a number: "))
-    
-    if user_operation == 6:
+    user_choice = int(input("Enter a number from 1 to 6: "))
+    if user_choice == 6:
         break
     
-    if user_operation not in [1,2,3,4,5,6]:
-        print(f"Invalid Input {user_operation}! Please select a number from the list")
-        continue
     
-    num1 = int(input("Enter number one: "))
-    num2 = int(input("Enter number two: "))
     
-    if user_operation == 1:
-        addition_result = addition(num1,num2)
-        print(f"The addition of {num1} and {num2} is: {addition_result}")       
-    elif user_operation == 2:
-        result = subtraction(num1, num2)
-        print(f"The subtraction of {num1} and {num2} is: {result}")
-    elif user_operation == 3:
-        result = multiplication(num1, num2)
-        print(f"The multiplication of {num1} and {num2} is: {result}")
-    elif user_operation == 4:
-        result = division(num1, num2)
-        print(f"The division result of {num1} and {num2} is {result}")
-    elif user_operation == 5:
-        result = power(num1, num2)
-        print(f"The number {num1} with power of {num2} is:  {result} ")
+    if user_choice == 1:
+        name = input("Enter student name: ").lower()
+        cgpa = float(input("Enter cgpa: "))
+        dept = input("Enter department name: ")
+        
+        student_info = {}
+        student_info["name"] = name
+        student_info["cgpa"] = cgpa
+        student_info["dept"] = dept
+        
+        result = add_std(student_info)
+
+        
+        
+    elif user_choice == 2:
+        view_std(student_list)
+        
+    
+    elif user_choice == 3:
+        std_to_search = input("Enter student name to search record: ").lower()
+        search_std(std_to_search)
+        
+        
+    elif user_choice == 4:
+        std_to_update_cgpa = input("Enter student name to update their CGPA: ").lower()
+        update_cgpa(std_to_update_cgpa)
+        
+        
+    elif user_choice == 5:
+        std_to_del = input("Enter student name to delete their record: ").lower()
+        delete_std(std_to_del)
--- 02_mini_projects/02_student_result_management_system/code.py
+++ 02_mini_projects/02_student_result_management_system/code.py
@@ -1,61 +1,66 @@
+# ========== Student Result Management ==========
+# 1. Add Student
+# 2. View All Students
+# 3. Search Student
+# 4. Update CGPA
+# 5. Delete Student
+# 6. Exit
+
 student_list = []
-
 def add_std(std):
-    student_list.append(std)
-       
+    print(f"{std} record added successfuly.")
+    return student_list.append(std)
     
-def view_std():
 
-    for std in student_list:               
-            for key, value in std.items():                
-                print(f"{key} : {value}")
-            print("-" * 30)
-                
-    if not student_list :
-        print("No records found!")
-       
-
-def search_std(name_to_search):
+def view_std(std):    
+    for student in student_list:
+        print(f"The records for {student["name"].title()}: ")
+        for key, value in student.items():
+            print(f"{key} : {value}")
+        print("-" * 30)
+    if not student_list:
+        print(f"No record found!")    
+        
+def search_std(std):
     found = False
-    
-    for std in student_list:
-        if std["name"].lower() == name_to_search:
-
-            print(f"The results for {std["name"]} are: ")
-                    
-            for key, value in std.items():
+    for student in student_list:
+        if student["name"].lower() == std:
+            print(f"{std.title()}'s record found: ")
+            for key, value in student.items():
                 print(f"{key} : {value}")
-                
             found = True
             break
     if not found:
-        print(f"{name_to_search}'s records not found.")  # Never print "Not found" while you're still searching. (not in the loop)
+        print(f"No record found for {std.title()}!")
+                
+def update_cgpa(std):
+    found = False
+    for student in student_list:
+        if student["name"].lower() == std:
+            updated_cgpa = float(input("Enter updated CGPA: "))
+            student["cgpa"] = updated_cgpa
+            print(f"{std.title()}'s CGPA updated successfully!")
+            found = True
+            break
+    if not found:
+        print(f"Invalid choice! {std.title()}'s record is not available. Please select from available records!")
+                      
+
+def delete_std(std):
+    found = False
+    for student in student_list:
+        if student["name"].lower() == std:
+            student_list.remove(student)
+            print(f"{student["name"].title()}'s record deleted successfully!")
+            found = True
+            break
+    if not found:
+        print(f"{std.title()}'s record didn't exists. Please select from avaialble records!")        
             
-
-def update_cgpa(std_name):
-    found = False
-    for std in student_list:
-        if std["name"].lower() == std_name:
-            std["cgpa"] = float(input("Enter updated CGPA: "))
-            print(f"{std['name']}'s CGPA updated successfully!")
-            found = True
-            break
-    if not found:
-        print(f"{std_name}'s record not found!")
-    
-
-def delete_records(std_to_del):
-    found = False
-    for std in student_list:
-        if std["name"].lower() == std_to_del:
-            student_list.remove(std)
-            print(f"{std_to_del}'s records deleted successfully!")
-            found = True
-            break
-    if not found:
-        print(f"{std_to_del}'s record not available.")
+             
 
 while True:
+    
     print("""
     ========== Student Result Management ==========
     1. Add Student
@@ -66,39 +71,40 @@ while True:
     6. Exit
     """)
     
-    user_input = int(input("Choose a number from 1 to 6 : "))
-    if user_input == 6:
+    user_choice = int(input("Enter a number from 1 to 6: "))
+    if user_choice == 6:
         break
     
-    student = {}
-    if user_input == 1:
-        name = input("Enter Student Name: ")
+    
+    
+    if user_choice == 1:
+        name = input("Enter student name: ").lower()
         cgpa = float(input("Enter cgpa: "))
-        dept = input("Enter dept name: ")
+        dept = input("Enter department name: ")
         
-        student["name"] = name
-        student["cgpa"] = cgpa
-        student["dept"] = dept  
-              
-        print(f"The record: {student} added successfully.")
-        add_std(student)
+        student_info = {}
+        student_info["name"] = name
+        student_info["cgpa"] = cgpa
+        student_info["dept"] = dept
+        
+        result = add_std(student_info)
 
         
-    elif user_input == 2:
-        view_std()
         
-    elif user_input == 3:
-        std_to_search = input("Enter Student name to search records: ").lower()
+    elif user_choice == 2:
+        view_std(student_list)
+        
+    
+    elif user_choice == 3:
+        std_to_search = input("Enter student name to search record: ").lower()
         search_std(std_to_search)
         
-    elif user_input == 4:
-        std_to_update_cgpa = input("Enter student name whos CGPA has to update: ").lower()
+        
+    elif user_choice == 4:
+        std_to_update_cgpa = input("Enter student name to update their CGPA: ").lower()
         update_cgpa(std_to_update_cgpa)
         
-    elif user_input == 5:
-        std_to_delete = input("Enter Student name whos records to delete: ").lower()
-        delete_records(std_to_delete)
         
-        
-    
-    
+    elif user_choice == 5:
+        std_to_del = input("Enter student name to delete their record: ").lower()
+        delete_std(std_to_del)
