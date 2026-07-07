acf7555c03625eab484d1e36a7aa56db62950a8a
Maria <mariafarooq164@gmail.com>
2026-07-07 20:42:12 +0500

    Reading files

--- 01_practice/8_file_handling.py
+++ 01_practice/8_file_handling.py
@@ -1,11 +1,37 @@
-file = open("hello.txt")
+# file = open("./01_practice/hello.txt")
 
 
-file_content = file.read()
-print(f"The file content is: {file_content}")
-print(type(file_content))
+# file_content = file.read()
+# print(f"The file content is: {file_content}")
+# print(type(file_content))
 
-file_content1 = file.read()
-print(f"The file content is: {file_content1}")
+# file_content1 = file.read()
+# print(f"The file content is: {file_content1}")
 
-file.close()
+# file.close()
+
+# when we use file.read() , python first check if the file is open or not. then condition2, checks where the pointer is?
+
+# file_content.read()  # will give error, bcz the file is already closed.
+
+# if the file is open but the pointer is at the end, then it returns an empty string.
+
+# with open("./01_practice/hello.txt") as file1:
+#     content = file1.read()
+    
+# print("new content: ")
+# print(content)
+
+# with open("./01_practice/hello.txt") as file3:
+#     line1 = file3.readline()
+    
+    
+# print(line1)
+
+with open("./01_practice/hello.txt") as file:
+    lines = file.readlines()
+    
+print(lines)
+print(len(lines))
+print(type(lines))
+
