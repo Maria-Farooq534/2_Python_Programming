98bd3ad86ce14de4aace2f12d5f21b2265408cc0
Maria <mariafarooq164@gmail.com>
2026-07-08 21:47:57 +0500

    writing in files

--- 01_practice/8_file_handling.py
+++ 01_practice/8_file_handling.py
@@ -28,10 +28,35 @@
     
 # print(line1)
 
-with open("./01_practice/hello.txt") as file:
-    lines = file.readlines()
+# with open("./01_practice/hello.txt") as file:
+#     lines = file.readlines()
     
-print(lines)
-print(len(lines))
-print(type(lines))
+# print(lines)
+# print(len(lines))
+# print(type(lines))
 
+# with open("./01_practice/hello.txt" , "w") as file_1:
+#     file_1.write("Hello! I'm writing in the file.")
+    
+# print(file_1)
+
+# with open("./01_practice/new_file.txt" , "w") as file_2:
+#     file_2.write("This is new text file.")
+    
+    
+# with open("./01_practice/hello.txt", "r") as file_3:
+#     file_2_content = file_3.read()
+# print(file_2_content)
+
+
+# with open("./01_practice/notes.txt" , "w") as file_4:
+#     file_4.write("First line of Notes Files.\n")
+#     file_4.write("Second line of File. \n")
+    
+# print(file_4)
+
+with open("notes.txt", "w") as file:
+    result = file.write("Hello")
+
+print(result)
+print(type(result))
