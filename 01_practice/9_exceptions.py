1cdb7ecb5f2e461097bbdbec5498332853090624
Maria <mariafarooq164@gmail.com>
2026-07-12 22:10:31 +0500

    try excepty

--- 01_practice/9_exceptions.py
+++ 01_practice/9_exceptions.py
@@ -1,3 +1,8 @@
-age = int(input("Enter your age: "))
+# age = int(input("Enter your age: "))
 
-print(age)
+# print(age)
+
+try:
+    result = 10 / 0
+except ZeroDivisionError:
+    print("Error: You cannot divide a number by zero!")
