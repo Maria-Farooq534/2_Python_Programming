d6f186a4ca615dfa884931ea9cac5b176524714b
Maria <mariafarooq164@gmail.com>
2026-07-16 17:51:35 +0500

    Project: Student Grade Manager

--- 01_practice/8_file_handling.py
+++ 01_practice/8_file_handling.py
@@ -1,57 +1,57 @@
-# file = open("./01_practice/hello.txt")
+file = open("./01_practice/hello.txt")
 
 
-# file_content = file.read()
-# print(f"The file content is: {file_content}")
-# print(type(file_content))
+file_content = file.read()
+print(f"The file content is: {file_content}")
+print(type(file_content))
 
-# file_content1 = file.read()
-# print(f"The file content is: {file_content1}")
+file_content1 = file.read()
+print(f"The file content is: {file_content1}")
 
-# file.close()
+file.close()
 
 # when we use file.read() , python first check if the file is open or not. then condition2, checks where the pointer is?
 
-# file_content.read()  # will give error, bcz the file is already closed.
+file_content.read()  # will give error, bcz the file is already closed.
 
 # if the file is open but the pointer is at the end, then it returns an empty string.
 
-# with open("./01_practice/hello.txt") as file1:
-#     content = file1.read()
+with open("./01_practice/hello.txt") as file1:
+    content = file1.read()
     
-# print("new content: ")
-# print(content)
+print("new content: ")
+print(content)
 
-# with open("./01_practice/hello.txt") as file3:
-#     line1 = file3.readline()
+with open("./01_practice/hello.txt") as file3:
+    line1 = file3.readline()
     
     
-# print(line1)
+print(line1)
 
-# with open("./01_practice/hello.txt") as file:
-#     lines = file.readlines()
+with open("./01_practice/hello.txt") as file:
+    lines = file.readlines()
     
-# print(lines)
-# print(len(lines))
-# print(type(lines))
+print(lines)
+print(len(lines))
+print(type(lines))
 
-# with open("./01_practice/hello.txt" , "w") as file_1:
-#     file_1.write("Hello! I'm writing in the file.")
+with open("./01_practice/hello.txt" , "w") as file_1:
+    file_1.write("Hello! I'm writing in the file.")
     
-# print(file_1)
+print(file_1)
 
-# with open("./01_practice/new_file.txt" , "w") as file_2:
-#     file_2.write("This is new text file.")
+with open("./01_practice/new_file.txt" , "w") as file_2:
+    file_2.write("This is new text file.")
     
     
-# with open("./01_practice/hello.txt", "r") as file_3:
-#     file_2_content = file_3.read()
-# print(file_2_content)
+with open("./01_practice/hello.txt", "r") as file_3:
+    file_2_content = file_3.read()
+print(file_2_content)
 
 
-# with open("./01_practice/notes.txt" , "w") as file_4:
-#     file_4.write("First line of Notes Files.\n")
-#     file_4.write("Second line of File. \n")
+with open("./01_practice/notes.txt" , "w") as file_4:
+    file_4.write("First line of Notes Files.\n")
+    file_4.write("Second line of File. \n")
     
 # print(file_4)
 
---
+++ 02_mini_projects/04_student_grade_manager/code.py
@@ -0,0 +1,81 @@
+# ========= Student Grade Manager =========
+
+# 1. Add Student
+# 2. View Students
+# 3. Clear Records
+# 4. Exit
+
+def add_std():
+    name = input("Enter your name: ")
+    while True:
+        try:
+            marks = int(input("Enter your marks in numbers: "))
+            break
+        except ValueError as e:
+            print("Invalid format! Please enter marks in digits.")
+        
+    record = f"{name} - {marks}\n"
+    with open("./02_mini_projects/04_student_grade_manager/student_record.txt" , "a") as file:
+        file.write(record)
+        # file.write(name)
+        # file.write(" - ")
+        # file.write(str(marks))
+        # file.write("\n")
+        print(f"{name}'s record added successfully.")
+    
+def view_std():
+    try:
+        with open("./02_mini_projects/04_student_grade_manager/student_record.txt" , "r") as file:
+            content = file.read()
+            if not content:
+                print("No record found!")
+            else:
+                print(content)
+    except FileNotFoundError:
+        print("No student record management file exists.")
+
+def clear_records():
+    try:
+        with open("./02_mini_projects/04_student_grade_manager/student_record.txt" , "w") as file:
+            print("All records cleared!")
+            pass
+    except FileNotFoundError:
+        print("No student record management file exists.")
+    
+             
+
+while True:
+    print("""
+    ========= Student Grade Manager =========
+
+    1. Add Student
+    2. View Students
+    3. Clear Records
+    4. Exit
+    """)
+    
+    try:
+        user_input = int(input("Enter a number from 1 to 4: "))
+        if user_input == 4:
+            break
+        
+        if user_input == 1:
+            add_std()
+            
+        elif user_input == 2:
+            view_std()
+            
+        elif user_input == 3:
+            while True:
+                confirm_user = input("Are you sure you want to clear the records? y/n: ").lower()
+                if confirm_user in ["n" , "no"]:
+                    print("No records cleared!")
+                    break
+                if confirm_user in ["yes" , "y"]:
+                    clear_records()
+                    break
+                else:
+                    print("Please select a valid option!")
+            
+    except ValueError:
+        print("Invalid choice! Enter a valid number.")
