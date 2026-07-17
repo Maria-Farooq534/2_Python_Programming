a5438da616bdc4cd9b98f7ef70751435d163ff32
Maria <mariafarooq164@gmail.com>
2026-07-17 17:54:49 +0500

    modeules

---
+++ 01_practice/10_modules/greetings.py
@@ -0,0 +1,24 @@
+# module is a python file that has python code.
+print("Hello")
+
+# def hello():
+#     print("Hello and welcome!")
+    
+def sum(a,b):
+    print(a + b)
+    
+    
+# count = 0
+
+# def hello():
+#     global count
+#     # count += 1
+#     print(count)
+    
+count = 10
+def hello():
+    new_count = count + 5
+    print(new_count)
+    
+# hello()
+# print(count)
---
+++ 01_practice/10_modules/main.py
@@ -0,0 +1,13 @@
+import greetings
+# greetings.hello()
+# greetings.hello()
+# greetings.hello()
+
+
+
+# import greetings
+# greetings.hello()
+# # greetings.sum(2, 4)
+# print(greetings)
+
+print(greetings.count)
