e87034d0d73653e4469f4c4c4b98ee93f6d30410
Maria <mariafarooq164@gmail.com>
2026-07-25 20:35:35 +0500

    Modules in Python

--- 01_practice/10_modules/greetings.py
+++ 01_practice/10_modules/greetings.py
@@ -1,33 +1,11 @@
-# module is a python file that has python code.
-print("Hello")
+""" 
+ This is greetings module.
+"""
 
-# def hello():
-#     print("Hello and welcome!")
-    
-def sum(a,b):
-    print(a + b)
-    
+print("Loading output...")
 
-numbers = [1,2,3]
-    
-    
-# count = 0
-
-# def hello():
-#     global count
-#     # count += 1
-#     print(count)
-    
-count = 10
 def hello():
-    new_count = count + 5
-    print(new_count)
+    print("Hello")
     
-# hello()
-# print(count)
-
-def increase(): # instead of chnaging count variable in each file. we can simply define a function for increment and call the function whenever we want to change or incremnt the vlaue.
-    global count
-    count += 1
-    print(count)
-    # return count
+if __name__ == "__main__":
+    hello()
--- 01_practice/10_modules/main.py
+++ 01_practice/10_modules/main.py
@@ -1,71 +1,87 @@
-# import greetings
-
-from greetings import count
-print(count)
-
-count = 50
-print(count)
-
-from greetings import numbers
-print(numbers)
-numbers.append(4)
-# print(numbers)
-# print(greetings.numbers)
-
-# greetings.hello()
-# greetings.hello()
-# greetings.hello()
-
-
-
-
-# import greetings
-# greetings.hello()
-# # greetings.sum(2, 4)
-# print(greetings)
-
-
-
-# count = 100
-# count = greetings.count
-# count = count + 5
+import greetings
+# print(__name__)
+# from greetings import count
 # print(count)
-# print(greetings.count)
 
-# greetings.count += 5  this is not the good way to modify the variable. 
+# count = 50
+# print(count)
+
+# from greetings import numbers
+# print(numbers)
+# numbers.append(4)
+# # print(numbers)
+# # print(greetings.numbers)
+
+# # greetings.hello()
+# # greetings.hello()
+# # greetings.hello()
 
 
-# print(greetings.count)
-# greetings.increase()
 
 
-# a = greetings
-# a.count = 50
-# print(a.count)
-# print(greetings.count)
+# # import greetings
+# # greetings.hello()
+# # # greetings.sum(2, 4)
+# # print(greetings)
 
-# print(a is greetings)
 
-# x = 2
-# y = x
-# print(x)
-# print(y)
-# print(x is y)
 
-a = [1,2,3]
-b = a.append(3)
-print(a)
-print(b)
+# # count = 100
+# # count = greetings.count
+# # count = count + 5
+# # print(count)
+# # print(greetings.count)
 
-a = [1,2,3]
-b = a
-c = a.copy()
-a.append(4)
+# # greetings.count += 5  this is not the good way to modify the variable. 
 
-print(a)
-print(b)
-print(c)
 
-print(a is b)
-print(b is a)
-print(c is a)
+# # print(greetings.count)
+# # greetings.increase()
+
+
+# # a = greetings
+# # a.count = 50
+# # print(a.count)
+# # print(greetings.count)
+
+# # print(a is greetings)
+
+# # x = 2
+# # y = x
+# # print(x)
+# # print(y)
+# # print(x is y)
+
+# a = [1,2,3]
+# b = a.append(3)
+# print(a)
+# print(b)
+
+# a = [1,2,3]
+# b = a
+# c = a.copy()
+# a.append(4)
+
+# print(a)
+# print(b)
+# print(c)
+
+# print(a is b)
+# print(b is a)
+# print(c is a)
+
+# from greetings import hello
+# hello()
+
+print("Main started")
+
+# def main():
+#     print("Main program execution")
+    
+# if __name__ == "__main__":
+#     main()
+
+print(__name__)
+print(greetings.__name__)
+print(greetings.__file__) # gives the file path
+print(greetings.__doc__) # returns the doc string that every module can have.
