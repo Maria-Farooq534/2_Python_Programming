890dc8655e574db156f124cd6dd512f92d11a5a7
Maria <mariafarooq164@gmail.com>
2026-07-31 21:21:20 +0500

    Objects Validation

--- 01_practice/practice.py
+++ 01_practice/practice.py
@@ -1,110 +1,47 @@
-# ========== Student Result Management ==========
-# 1. Add Student
-# 2. View All Students
-# 3. Search Student
-# 4. Update CGPA
-# 5. Delete Student
-# 6. Exit
+# Create a Student class with __init__().
+# Add __str__().
+# Add validation using TypeError and ValueError.
+# Create four students:
+# valid integer marks
+# negative marks
+# marks greater than 100
+# string marks
+# Observe which ones are created and which ones fail.
 
-student_list = []
-def add_std(std):
-    print(f"{std} record added successfuly.")
-    return student_list.append(std)
-    
+class Student():
+    def __init__(self, name, marks):
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        if not name.strip():
+            raise ValueError("Name cannot be empty.")
+        
+        
+        self.name = name
+        
+        if not isinstance(marks, int):
+            raise TypeError("Marks must be in numbers")
+        elif marks < 0 or marks > 100:
+            raise ValueError("Marks must be between 0 and 100.")
+        
+        self.marks = marks
+        
+        
+    def __str__(self):
+        return f"Name: {self.name}\nMarks: {self.marks}\n{"*" * 25}"
+        
 
-def view_std(std):    
-    for student in student_list:
-        print(f"The records for {student["name"].title()}: ")
-        for key, value in student.items():
-            print(f"{key} : {value}")
-        print("-" * 30)
-    if not student_list:
-        print(f"No record found!")    
-        
-def search_std(std):
-    found = False
-    for student in student_list:
-        if student["name"].lower() == std:
-            print(f"{std.title()}'s record found: ")
-            for key, value in student.items():
-                print(f"{key} : {value}")
-            found = True
-            break
-    if not found:
-        print(f"No record found for {std.title()}!")
-                
-def update_cgpa(std):
-    found = False
-    for student in student_list:
-        if student["name"].lower() == std:
-            updated_cgpa = float(input("Enter updated CGPA: "))
-            student["cgpa"] = updated_cgpa
-            print(f"{std.title()}'s CGPA updated successfully!")
-            found = True
-            break
-    if not found:
-        print(f"Invalid choice! {std.title()}'s record is not available. Please select from available records!")
-                      
 
-def delete_std(std):
-    found = False
-    for student in student_list:
-        if student["name"].lower() == std:
-            student_list.remove(student)
-            print(f"{student["name"].title()}'s record deleted successfully!")
-            found = True
-            break
-    if not found:
-        print(f"{std.title()}'s record didn't exists. Please select from avaialble records!")        
-            
-             
+student1 = Student("Maria" , 90)
+print(student1)
+# student2 = Student("Maria" , -90)
+# print(student2)
+# student4 = Student("Maria" , 190)
+# print(student4)
+# student5 = Student("Maria" , "ninety")
+# print(student5)
 
-while True:
-    
-    print("""
-    ========== Student Result Management ==========
-    1. Add Student
-    2. View All Students
-    3. Search Student
-    4. Update CGPA
-    5. Delete Student
-    6. Exit
-    """)
-    
-    user_choice = int(input("Enter a number from 1 to 6: "))
-    if user_choice == 6:
-        break
-    
-    
-    
-    if user_choice == 1:
-        name = input("Enter student name: ").lower()
-        cgpa = float(input("Enter cgpa: "))
-        dept = input("Enter department name: ")
+student3 = Student("Noor" , 90)
+print(student3)
         
-        student_info = {}
-        student_info["name"] = name
-        student_info["cgpa"] = cgpa
-        student_info["dept"] = dept
-        
-        result = add_std(student_info)
-
-        
-        
-    elif user_choice == 2:
-        view_std(student_list)
-        
-    
-    elif user_choice == 3:
-        std_to_search = input("Enter student name to search record: ").lower()
-        search_std(std_to_search)
-        
-        
-    elif user_choice == 4:
-        std_to_update_cgpa = input("Enter student name to update their CGPA: ").lower()
-        update_cgpa(std_to_update_cgpa)
-        
-        
-    elif user_choice == 5:
-        std_to_del = input("Enter student name to delete their record: ").lower()
-        delete_std(std_to_del)
+student3.marks = 500
+print(student3)
