9a434e9d8cd263b750799434b36ad7af0db6bd35
Maria <mariafarooq164@gmail.com>
2026-08-05 17:49:12 +0500

    method lookup in inheritance

---
+++ 01_practice/15_inheritance.py
@@ -0,0 +1,55 @@
+# class Person():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+        
+    
+#     def __str__(self):
+#         return f"Name: {self.name}\nAge: {self.age}\nMarks: {self.marks}\n{"*" * 25}"
+        
+        
+#     def introduce(self):
+#         print(f"My name is {self.name}!")
+    
+
+# class Student(Person):
+#     def __init__(self, name, age, marks):
+#         super().__init__(name, age)
+#         self.marks = marks
+        
+
+# student1 = Student("Maria" , 24, 90)
+# print(student1)
+
+
+# """
+# if a child class has its own constructor, it overrides the parent class constructor __init__ .
+
+# """
+
+# class Person:
+#     def introduce(self):
+#         print("Hello")
+
+# class Student: 
+#     def introduce(self):
+#         super().__init__()
+#         print("I am a doctor.")
+#         super().__init__()
+        
+
+# student = Student()     
+# student.introduce()
+
+
+class Person:
+    def walk(self):
+        print("Walking...")
+
+
+class Student(Person):
+    pass
+
+student = Student() 
+student.walk()
+    
