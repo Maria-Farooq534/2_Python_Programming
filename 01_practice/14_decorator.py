e30ab8190c4764cd3ce0089a4d9a6a3b5ce36807
Maria <mariafarooq164@gmail.com>
2026-08-31 20:04:26 +0500

    decorators

--- 01_practice/14_decorator.py
+++ 01_practice/14_decorator.py
@@ -56,3 +56,151 @@ But Python programmers understand:
 "I shouldn't do this unless I have a very good reason."
 
 """
+
+class Employee():
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+    
+    def __str__(self):
+        return f"\nName: {self.name} \nSalary: {self.salary}" 
+    
+    
+    @property
+    def salary(self):
+        return self._salary
+    
+    @salary.setter
+    def salary(self, value):
+        if not isinstance(value , (int, float)):
+            raise TypeError("Salary must be in integer or float.")
+        
+        
+        if value < 0 or value > 100000000:
+            raise ValueError("Salary must be in between 10000 to 100000000.")
+        
+        # if not isinstance(value , float):
+        #     raise TypeError("Salary must be in integer or float.")
+        
+        self._salary = value  
+  
+# 1              
+employee1 = Employee("Maria" , 900000.0)        
+print(employee1.name)
+print(employee1.salary)
+print(employee1)
+print(employee1.__dict__)
+
+
+employee1.salary = 99000876
+print(employee1)
+print(employee1.__dict__)
+
+
+employee1._salary = 9657676
+print(employee1)
+print(employee1.__dict__)
+
+# 2
+
+e1 = Employee("Maria", 50000)
+
+print(e1.salary)
+
+e1.salary = 60000
+print(e1.salary)
+
+e1.salary = 600000-500
+print(e1.salary)
+
+e1.salary = 500 + 600000
+print(e1.salary)
+
+e1.salary = True
+print(e1.salary)
+# e1.salary = "60000"
+# e1.salary = None
+
+
+# practice challenge — Product
+# Create a class called Product.
+# It should have:
+# name
+# price
+# stock
+# Requirements:
+# name should be stored normally.
+
+# price must use @property + @price.setter.
+# It must accept int or float.
+# It must be between 0 and 1,000,000.
+# Otherwise raise an appropriate error.
+
+# stock must also use @property + @stock.setter.
+# It must be an integer.
+# It cannot be negative.
+# Otherwise raise an appropriate error.
+
+# Store the actual values internally as:
+# _price
+# _stock
+# Add __str__() to display the product information.
+# Create two products with different values.
+# Test:
+# reading product.price
+# changing product.price
+# reading product.stock
+# changing product.stock
+# an invalid price
+# an invalid stock
+# directly changing _price once, just to remind yourself that _price is still technically accessible.
+# Solution:
+
+class Product:
+    def __init__(self, name, price, stock):
+        self.name = name
+        self.price = price 
+        self.stock = stock
+        
+    def __str__(self):
+        return f"\nName: {self.name} \nPrice: {self.price} \nStock: {self.stock}"
+    
+    def __len__(self):
+        return len(self.__dict__)
+    
+    # Price
+
+    @property
+    def price(self):
+        return self._price
+        
+    @price.setter
+    def price(self, price_value):
+        if not isinstance(price_value , (int, float)):
+            raise TypeError("Price must be integer or float.")
+        
+        if price_value < 0 or price_value > 1000000:
+            raise ValueError("Price must be in between 0 and 1000000.")
+        
+        self._price = price_value
+        
+    # Stock
+    @property
+    def stock(self):
+        return self._stock
+    
+    @stock.setter
+    def stock(self, stock_value):
+        if not isinstance(stock_value , (int, float)):
+            raise TypeError("Stock must be an integer or float.")
+        
+        if stock_value < 0:
+            raise ValueError("Stock  can't be a negative number.")
+    
+        self._stock = stock_value
+        
+    
+price1 = Product("Laptop" , 900000 , 90)
+print(price1)
+price1._stock = -90
+print(price1)
--- 01_practice/16_method_overriding.py
+++ 01_practice/16_method_overriding.py
@@ -20,3 +20,74 @@ student.introduce()
 
 # MRO
 print(Researcher.__mro__)
+
+
+
+# practice challenge
+# Model
+#  ├── LinearRegression
+#  ├── DecisionTree
+#  └── NeuralNetwork
+
+# Requirements:
+
+# Model should have:
+# predict()
+# that prints something generic.
+# Each child class should override predict() and print its own message.
+# Create one object from each class.
+# Call:
+# linear.predict()
+# tree.predict()
+# neural.predict()
+# Do not use super() yet.
+
+class Model:
+    def predict(self):
+        print("ML Model.")
+
+class LinearRegression(Model):
+    def predict(self):
+        print("Linear regression model prediction.")
+        
+
+class DecisionTree(Model):
+    
+    def predict(self):
+        print("Classification Model.")
+        super().predict()
+
+        
+class NeuralNetwork(Model):
+    def predict(self):
+        print("Used for Images, Audio, Videos.")
+        
+        
+model = Model()
+lin_reg = LinearRegression()
+d_tree = DecisionTree()
+n_network = NeuralNetwork()
+
+
+# model.predict()
+# lin_reg.predict()
+# d_tree.predict()
+# n_network.predict()
+
+
+
+models = [model, lin_reg, d_tree, n_network]
+
+for model in models:
+    model.predict()
+    
+
+print(NeuralNetwork.__mro__)
+
+
+print(isinstance(model, Model))
+print(isinstance(lin_reg, LinearRegression))
+
+print(issubclass(LinearRegression, Model)) # True
+print(issubclass(DecisionTree, Model))   # True
+print(issubclass(Model, DecisionTree))   # False
