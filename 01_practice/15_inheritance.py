8388eb8b740b68627272901b23a84f2fa6f45de3
Maria <mariafarooq164@gmail.com>
2026-09-03 20:07:07 +0500

    Inheritance

--- 01_practice/15_inheritance.py
+++ 01_practice/15_inheritance.py
@@ -78,3 +78,40 @@ print(student1)
 print(student2)
 
     
+# Parameters
+
+
+class Parameters:
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
+    
+    
+class Bias(Parameters):
+    def __init__(self, name, number, bias_value):
+        super().__init__(name, number)
+        self.bias_value = bias_value
+        
+class Weights(Parameters):
+    def __init__(self, name, number, weight):
+        super().__init__(name, number)
+        self.weight = weight
+        
+
+parameter = Parameters("parameters" , '2')
+bias = Bias("Bias" , 1 , 0.1)
+weights = Weights("Weight" , 2 , '10')
+
+print("Parameters")
+print(parameter.name)
+print(parameter.number)
+
+print("Bias")
+print(bias.name)
+print(bias.number)
+print(bias.bias_value)
+
+print("Weights")
+print(weights.name)
+print(weights.number)
+print(weights.weight)
