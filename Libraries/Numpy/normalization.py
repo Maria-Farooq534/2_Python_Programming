7bbac0a2b4c18d9eeefa4e64fe2e0165ef93c605
Maria <mariafarooq164@gmail.com>
2026-09-22 20:09:16 +0500

    normalization methods

--- Libraries/Numpy/normalization.py
+++ Libraries/Numpy/normalization.py
@@ -7,3 +7,21 @@ X = np.array([
     [1800, 4, 3],
     [1100, 3, 15]
 ])
+
+print(np.mean(X, axis=0))
+print(np.std(X, axis=0))
+
+print(X.ndim)
+print(X.shape)
+
+def zscore_normalization(x):
+    x_mean = np.mean(x, axis=0)
+    x_std = np.std(x , axis=0)
+    x_norm = (x - x_mean ) / x_std
+    return x_norm
+
+print(X)
+x_scaled = zscore_normalization(X)
+print(x_scaled)
+print(np.mean(x_scaled, axis=0))
+print(np.std(x_scaled, axis=0))
